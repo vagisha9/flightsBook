6906a0195658e3bbc390983264fe25ce82e1e344
office <vagisha14@gmail.com>
2017-08-29 13:00:51 +0400

    Commit flights

---
+++ flightsbooking/src/test/java/com/expedia/flightsbooking/TestNG_Grouping.java
@@ -0,0 +1,38 @@
+package com.expedia.flightsbooking;
+
+//Run in Maven:  maven test -PRegressionTest
+
+import org.testng.annotations.*;
+
+public class TestNG_Grouping {
+	
+	@BeforeClass(alwaysRun=true)
+	public void beforeClass() {
+		System.out.println("Before Class");
+	}
+	
+	@Test(groups= {"cars","SUV"})
+	public void testBMWX6() {
+		System.out.println("Running Test - BMW X6");
+	}
+	
+	@Test(groups= {"cars","Sedan"})
+	public void testAudiA6() {
+		System.out.println("Running Test - Audi A6 ");
+	}
+	
+	@Test(groups= {"Bikes"})
+	public void testNinja() {
+		System.out.println("Running Test - Kawasaki Ninja");
+	}
+	
+	@Test(groups= {"Bikes"})
+	public void testHondaCBR() {
+		System.out.println("Running Test - Honda CBR");
+	}
+
+	@AfterClass(alwaysRun=true)
+	public void afterClass() {
+		System.out.println("After Class");
+	}
+}
